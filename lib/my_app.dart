4881bfc8f6b1edfe4e43fcfea0b1a00440d620ec
Mohamed Elshafei <mohamedelshafei837@gmail.com>
2025-11-03 22:14:18 +0100

    finish appRoutes and seprate myApp Class

---
+++ lib/my_app.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'core/routes/app_routes.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp.router(
+      routerConfig:AppRoutes.router,
+      debugShowCheckedModeBanner:false,
+    );
+  }
+}
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,9 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:round_7_mobile_cure_team3/my_app.dart';
 
-
-import '../lib/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
